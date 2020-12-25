e3a159645550a65813bd28d1cdc83083beba6216
irkaal <irkaalv@gmail.com>
2020-12-24 17:28:41 -0800

    Add files

---
+++ README.md
@@ -0,0 +1,24 @@
+# English Premier League Results from 1993-94 to 2020-21
+
+## Introduction
+
+This repository contains an R script for scraping historical EPL match results. The dataset is available for download on [Kaggle](https://www.kaggle.com/irkaal/english-premier-league-results).
+
+## Scraping
+
+You can scrape the dataset from
+[Football-Data.co.uk](https://football-data.co.uk) with:
+
+```r
+# install.packages(
+#   c(
+#     "arrow",
+#     "furrr",
+#     "tidyverse",
+#     "usethis"
+#   )
+# )
+source("results.R")
+```
+
+The csv and parquet files are stored in the data folder.
---
+++ results.R
@@ -0,0 +1,86 @@
+suppressPackageStartupMessages({
+  library(arrow)
+  library(dplyr)
+  library(furrr)
+  library(lubridate)
+  library(purrr)
+  library(readr)
+  library(rvest)
+  library(stringr)
+  library(tidyr)
+  library(usethis)
+})
+
+ui_info("Retrieving paths...")
+paths <-
+  read_html("https://www.football-data.co.uk/englandm.php") %>%
+  html_nodes(css = "a:contains('Premier League')") %>%
+  html_attr("href") %>%
+  map_chr( ~ str_c("https://www.football-data.co.uk/", .)) %>%
+  rev()
+ui_info("OK")
+
+ui_info("Retrieving results...")
+plan(multisession)
+read <- function(path) {
+  suppressWarnings(
+    read_csv(
+      path,
+      col_types = cols(
+        .default = col_skip(),
+        Div = col_skip(),
+        Date = col_character(),
+        Time = col_character(),
+        HomeTeam = col_character(),
+        AwayTeam = col_character(),
+        FTHG = col_integer(),
+        FTAG = col_integer(),
+        FTR = col_character(),
+        HTHG = col_integer(),
+        HTAG = col_integer(),
+        HTR = col_character(),
+        Referee = col_character(),
+        HS = col_integer(),
+        AS = col_integer(),
+        HST = col_integer(),
+        AST = col_integer(),
+        HC = col_integer(),
+        AC = col_integer(),
+        HF = col_integer(),
+        AF = col_integer(),
+        HY = col_integer(),
+        AY = col_integer(),
+        HR = col_integer(),
+        AR = col_integer()
+      )
+    ) %>%
+      drop_na(Date) %>%
+      mutate(
+        Season = str_sub(path,-11L,-10L) %>%
+          as_date(format = "%y") %>%
+          year() %>%
+          str_c("-", str_sub(path,-9L,-8L))
+      )
+  )
+}
+results <- future_map_dfr(paths, read, .progress = TRUE)
+cat("\n")
+ui_info("OK")
+
+ui_info("Parsing dates...")
+results <- results %>%
+  mutate(
+    DateTime = Date %>%
+      str_c(replace_na(Time, ""), sep = " ") %>%
+      parse_date_time(c("dmy", "dmy HM"), tz = "GMT"),
+    Date = NULL,
+    Time = NULL
+  ) %>%
+  select(Season, DateTime, everything())
+ui_info("OK")
+
+ui_info("Saving results...")
+unlink("data/*", recursive = TRUE)
+write_parquet(results, "data/results.parquet")
+write_csv(results, "data/results.csv")
+ui_info("OK")
